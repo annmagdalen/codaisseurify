1ed171e1b49f26ff784209e042bdb2435a3594bc
annmagdalen <annmagdalen@hotmail.com>
2017-05-25 14:18:25 +0200

    Add Front Page with list of artists

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "artists#index"
 
-  resources :artists, only: [:show]
+  resources :artists, only: [:index, :show, :delete]
+  resources :songs, only: [:new, :create, :delete]
 end
